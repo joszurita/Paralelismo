b2e582ed877b05cd7cbcc245d4b681e902ac9fca
joszurita <52212653+joszurita@users.noreply.github.com>
2020-08-13 21:45:11 -0500

    Ya subio

---
+++ PasarImagenesSolucion/PasarImagenes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PasarImagenes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path1 = @"C:\Users\FRANCISCO RIERA\source\repos\PasarImagenesSolucion\PasarImagenes\imagenes1\";
+            string path2 = @"C:\Users\FRANCISCO RIERA\source\repos\PasarImagenesSolucion\PasarImagenes\imagenes2\";
+
+            string[] lst = Directory.GetFiles(path1);
+
+            int a = 1;
+            foreach(var archivo in lst)
+            {
+                File.Copy(archivo, path2 + "a" + a + ".jpg");
+                a++;
+                Console.WriteLine("se paso ya sin paralelo ¡¡¡¡¡¡");
+            }
+
+
+            Parallel.ForEach(lst, (archivo) =>
+            {
+                File.Copy(archivo, path2 + "a" + a + ".jpg");
+                a++;
+                Console.WriteLine("se paso ya con paralelo ¡¡¡¡¡¡");
+            });
+        }
+    }
+}
